104ba998625d21bc2188dc39a92d7d6e593b7db1
Noe10 <escorpio.rosa.94@gmail.com>
2019-05-14 11:39:51 -0500

    Hola

---
+++ Hola.java
@@ -0,0 +1,8 @@
+public class Hola{
+	public static void main (String [] args){
+		System.out.println("¡BIENVENIDOS!!");
+System.out.println("Ahora estamos en el Módulo IV.");
+System.out.println("Prometo esforzarme día a día, mucho más que el módulo ");
+System.out.println("anterior.");
+	}
+}
